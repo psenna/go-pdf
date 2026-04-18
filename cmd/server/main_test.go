74db35f9dd2eac956fa119ec2e7052e6b2f097fc
psenna <psenna@psenna.com.br>
2026-04-18 00:00:56 +0000

    clean small qwen2.5 code errors

--- cmd/server/main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"testing"
-)
-
-func TestMainCompiles(t *testing.T) {
-	// Just verify the main package compiles
-}
--- internal/handlers/index.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func IndexHandler(c *gin.Context) {
-	c.File("public/index.html")
-}
