2e977c92cb5b187de8c98c1d09593523009cf799
psenna <psenna@psenna.com.br>
2026-04-20 00:02:43 +0000

    feat: add PDF optimization logic using pdfcpu

--- internal/pdf/shrink_test.go
+++ internal/pdf/shrink_test.go
@@ -1,12 +1,30 @@
 package pdf
 
 import (
+	"os"
+	"os/exec"
 	"testing"
+
+	"github.com/psenna/go-pdf/internal/tempfile"
 )
 
-func TestErrorsExist(t *testing.T) {
-	var _ = ErrInvalidPDF
-	var _ = ErrFileTooLarge
-	var _ = ErrProcessingError
-	var _ = ErrResourceExhausted
+func TestOptimize(t *testing.T) {
+	// Skip if pdfcpu not installed
+	if _, err := exec.LookPath("pdfcpu"); err != nil {
+		t.Skip("pdfcpu not installed")
+	}
+
+	tmpFile, err := tempfile.CreateTempFile([]byte("%PDF-1.4..."))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpFile)
+
+	result, err := Optimize(tmpFile)
+	if err != nil {
+		t.Error(err)
+	}
+	if result != tmpFile+".optimized" {
+		t.Error("expected optimized file path")
+	}
 }
